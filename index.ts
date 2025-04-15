581f1584ad96f1baf9aeed6456ad37cb597f053f
root <sissonjack67@gmail.com>
2025-04-15 23:47:28 +0000

    feat: fix math

--- index.ts
+++ index.ts
@@ -458,7 +458,7 @@ async function main() {
       continue;
     }
 
-    const percentOfPool = results.silkAmount / results.stabilityPoolAmount;
+    const percentOfPool = results.silkAmount / (results.stabilityPoolAmount * 10**6);
     const collateralRaw = (Number(reward[1]) / percentOfPool) / 0.97;
     const collateralToken = tokenBody.data.tokens.find(
       (token) => token.contractAddress === reward[0].contract.address
@@ -466,7 +466,7 @@ async function main() {
     if(!collateralToken) {
       continue;
     }
-    const collateralAmount = collateralRaw * (10 ** collateralToken.Asset.decimals);
+    const collateralAmount = collateralRaw / (10 ** collateralToken.Asset.decimals);
     const price = priceBody?.data?.prices?.find((apiPrice) => {
       const tokenPriceIds = collateralToken?.PriceToken.map(
         (priceToken) => priceToken.priceId
